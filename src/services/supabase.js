32748f8c9493e3cb5d8bfc76abc0d227acc11f75
Mikyas Moges <88343655+mikimoges@users.noreply.github.com>
2025-01-02 10:43:39 +0300

    Update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://wlzeuufmlymogunryrzu.supabase.co";
-// const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsemV1dWZtbHltb2d1bnJ5cnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MTQwODcsImV4cCI6MjA0ODI5MDA4N30.o-cElvmx9-xcNk29AWgsxo4fmTGLa5EXSB4e9TbEnmg";
+export const supabaseUrl = "URL";
+
+const supabaseKey ="YOUR-KEY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
